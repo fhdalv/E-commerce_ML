41aceef39630786c7148cefa484935a39496828f
Emel <emel@Emels-MacBook-Air.local>
2017-10-18 10:40:00 -0400

    added dev database, cleaned up bootstrap code

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://ecommteam:ecommteam@ds161069.mlab.com:61069/cunycode_ecomm'}
-} else {
-  module.exports = {mongoURI: 'mongodb://localhost/loginapp'}
+module.exports = {
+  mongoURI: 'mongodb://ecommteam:ecommteam@ds161069.mlab.com:61069/cunycode_ecomm'
 }
+
+
